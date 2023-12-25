2105793abaec0b26f60b2d163a4c834c27f40a93
fabriciocr2 <fabriciocr1980@hotmail.com>
2023-12-25 18:16:39 -0300

    first-commit

---
+++ assets/js/revel.js
@@ -0,0 +1,61 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal(".efeito-text-topo", {
+  duration: 2000,
+  distance: "9rem",
+});
+revelar.reveal(".efeito-img-topo", {
+  duration: 1500,
+  distance: "15rem",
+  delay: 500,
+});
+
+revelar.reveal(".efeito-sobrenos-img", {
+  duration: 1000,
+  distance: "15rem",
+  delay: 500,
+});
+
+revelar.reveal(".efeito-sobrenos-text", {
+  duration: 1000,
+  distance: "9rem",
+  delay: 600,
+});
+
+revelar.reveal(".efeito-servicos-text", {
+  duration: 1000,
+  distance: "9rem",
+  delay: 500,
+});
+
+revelar.reveal(".efeito-servicos-img", {
+  duration: 1000,
+  distance: "15rem",
+  delay: 500,
+});
+revelar.reveal(".efeito-form", {
+  duration: 1000,
+  distance: "9rem",
+  delay: 500,
+});
+revelar.reveal(".efeito-inputs", {
+  duration: 1000,
+  distance: "15rem",
+  delay: 500,
+});
+
+revelar.reveal(".efeito-rodape-1", {
+  duration: 1000,
+  distance: "9rem",
+  delay: 500,
+});
+revelar.reveal(".efeito-rodape-2", {
+  duration: 1000,
+  distance: "9rem",
+  delay: 500,
+});
+revelar.reveal(".efeito-rodape-3", {
+  duration: 1000,
+  distance: "9rem",
+  delay: 500,
+});
